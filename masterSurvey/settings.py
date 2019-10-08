3a3dcb25a69f6fb6fb301d16e53721a76a45230c
Christiano Rossini <christiano.rossini.mc@gmail.com>
2019-10-08 13:02:18 -0300

    config

--- masterSurvey/settings.py
+++ masterSurvey/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'masterSurvey.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+
+""" DATABASES = {
+     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    } 
+} """
+
+DATABASES = {   
+    'default': {     
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "masterquest",
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
     }
 }
 
