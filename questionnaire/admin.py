a4d77dc6696b416e141fb0ce3c4b6372877db90a
Christiano Rossini <christiano.rossini.mc@gmail.com>
2019-12-30 22:17:18 -0300

    commit

--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -6,9 +6,22 @@ from .models import Questionnaire, AnswerTaskID, Answer, LatinSquare
 from .models import Task, DTModel
 
 admin.site.register(Participant)
-admin.site.register(Task)
-admin.site.register(DTModel)
+#admin.site.register(Task)
+#admin.site.register(DTModel)
 admin.site.register(Questionnaire)
 admin.site.register(AnswerTaskID)
 admin.site.register(Answer)
-admin.site.register(LatinSquare)
+#admin.site.register(LatinSquare)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display =  [f.name for f in Task._meta.fields if f.name != "codeSnippetContent"]    
+admin.site.register(Task, TaskAdmin)
+
+class DTModelAdmin(admin.ModelAdmin):
+    list_display =  [f.name for f in DTModel._meta.fields]    
+admin.site.register(DTModel, DTModelAdmin)
+
+class LatinSquareAdmin(admin.ModelAdmin):
+    list_display =  [f.name for f in LatinSquare._meta.fields]    
+admin.site.register(LatinSquare, LatinSquareAdmin)
--- questionnaire/models.py
+++ questionnaire/models.py
@@ -144,11 +144,12 @@ class Task(models.Model):
         return "Id: {3}, Task Group: {0}, CS scope: {1}, CS Type: {2}".format(self.taskGroup, self.codeSmellScope, self.codeSmellType, self.id)
     
     def getCsDescription(self):
-        csDescriptions = {'gc':'GOD CLASS;A large class implementing different responsibilities and centralizing most of the system processing.',
-                            'lpl':'LONG PARAMETER LIST;A method having a long list of parameters, some of which avoidable.',
-                            'lm':'LONG METHOD;A method that is unduly long in terms of lines of code.',
-                            'mm':'MIDDLE MAN;A class delegating to other classes most of the methods it implements.',
+        csDescriptions = {'gc':'GOD CLASS;A large class implementing different responsibilities and centralizing most of the system processing.',                            
+                            'mm':'MIDDLE MAN;A class delegating to other classes most of the methods it implements. Middle Man instances arise when a class is delegating all its work to other classes.',
                             'cdsbp':'CLASS DATA SHOULD BE PRIVATE;A class exposing its fields, violating the principle of data hiding.',
+                            'fe':'FEATURE ENVY;Refers to methods that use much more data from other classes than from their own class. A Feature Envy tends to use more attributes from other classes than from its own class, and to use many attributes from few different classes',
+                            'lpl':'LONG PARAMETER LIST;A method having a long list of parameters, some of which avoidable.',
+                            'lm':'LONG METHOD;A method that is unduly long in terms of lines of code. A method that is too long and tries to do too much',
                             }
         return csDescriptions[self.codeSmellType].split(';')
 
